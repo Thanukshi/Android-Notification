ba910a911a6c45727eb51d4fb469dbafceb2c428
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-03-27 19:15:54 +0530

    Get a Notification

--- app/src/main/java/com/example/androidnotification/Signup.java
+++ app/src/main/java/com/example/androidnotification/Signup.java
@@ -1,9 +1,14 @@
 package com.example.androidnotification;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,11 +19,12 @@ import android.widget.EditText;
 public class Signup extends AppCompatActivity {
 
 
-
+    private static final String CHANNEL_ID = "1";
     EditText view;
     Button signUp;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +35,6 @@ public class Signup extends AppCompatActivity {
 
         signUp.setOnClickListener(new View.OnClickListener() {
 
-            private static final String CHANNEL_ID = "1" ;
 
             @Override
             public void onClick(View v) {
@@ -39,27 +44,39 @@ public class Signup extends AppCompatActivity {
                 startActivity(send);
 
 
-                    // Create the NotificationChannel, but only on API 26+ because
-                    // the NotificationChannel class is new and not in the support library
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        CharSequence name = getString(R.string.channel_name);
-                        String description = getString(R.string.channel_description);
-                        int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-                        channel.setDescription(description);
-                        // Register the channel with the system; you can't change the importance
-                        // or other notification behaviors after this
-                        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-                        notificationManager.createNotificationChannel(channel);
-                 
-                }
-
+                // Create the NotificationChannel, but only on API 26+ because
+                // the NotificationChannel class is new and not in the support library
             }
+
         });
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = getString(R.string.channel_name);
+                String Description = getString(R.string.channel_description);
 
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                channel.setDescription(Description);
 
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
 
+            Intent intent = new Intent(this,Notification.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("New Notification")
+                    .setContentText("You are signing now")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+            notificationManager.notify(0, builder.build());
+        }
     }
-}
+
