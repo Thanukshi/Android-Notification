77aab326929751d9a4493d745ac89e217bca032c
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-03-27 18:24:55 +0530

    Get a Notification

--- app/src/main/java/com/example/androidnotification/Signup.java
+++ app/src/main/java/com/example/androidnotification/Signup.java
@@ -2,11 +2,14 @@ package com.example.androidnotification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Signup extends AppCompatActivity {
+
 
     EditText view;
     Button signUp;
@@ -17,7 +20,19 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-       view = findViewById(R.id.btn_Main);
+        view = findViewById(R.id.ET_main);
+        signUp = findViewById(R.id.btn_Main);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent send = new Intent(Signup.this, Register.class);
+                String name = view.getText().toString();
+                send.putExtra("FullName", name);
+                startActivity(send);
+            }
+        });
+
 
     }
 }
