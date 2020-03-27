27b96f9d452f5f86fe336fe0fdc79f87ffd650f8
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-03-27 18:52:42 +0530

    Get a Notification

--- app/src/main/java/com/example/androidnotification/Signup.java
+++ app/src/main/java/com/example/androidnotification/Signup.java
@@ -38,17 +38,20 @@ public class Signup extends AppCompatActivity {
                 send.putExtra("FullName", name1);
                 startActivity(send);
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    CharSequence name = getString(R.string.ID1_MainJava);
-                    String description = getString(R.string.ID2_MainJava);
-                    int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-                    channel.setDescription(description);
-                    // Register the channel with the system; you can't change the importance
-                    // or other notification behaviors after this
-                    NotificationManager notificationManager =
-                            getSystemService(NotificationManager.class);
-                    notificationManager.createNotificationChannel(channel);
+
+                    // Create the NotificationChannel, but only on API 26+ because
+                    // the NotificationChannel class is new and not in the support library
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        CharSequence name = getString(R.string.channel_name);
+                        String description = getString(R.string.channel_description);
+                        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                        channel.setDescription(description);
+                        // Register the channel with the system; you can't change the importance
+                        // or other notification behaviors after this
+                        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                        notificationManager.createNotificationChannel(channel);
+                 
                 }
 
             }
